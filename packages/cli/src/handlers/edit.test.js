307d259969a9949499f9db2563ef5a564bb1e928
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2025-12-01 11:53:23 +0900

    fixup! test(cli): make edit handler happy paths assert exit stubs

--- packages/cli/src/handlers/edit.test.js
+++ packages/cli/src/handlers/edit.test.js
@@ -220,15 +220,15 @@ describe(
         },
       }));
 
-    await rejects(
-      async () => await editHandler(["docs", "README.md"]),
-      /Process exit with code 0/,
-    );
+      await rejects(
+        async () => await editHandler(["docs", "README.md"]),
+        /Process exit with code 0/,
+      );
 
-    const spawnCall = spawnMock.mock.calls[0].arguments;
-    strictEqual(spawnCall[0], "vim");
-    strictEqual(spawnCall[1][0], "README.md");
-    strictEqual(spawnCall[2].cwd, "/repo/.git/phantom/worktrees/docs");
+      const spawnCall = spawnMock.mock.calls[0].arguments;
+      strictEqual(spawnCall[0], "vim");
+      strictEqual(spawnCall[1][0], "README.md");
+      strictEqual(spawnCall[2].cwd, "/repo/.git/phantom/worktrees/docs");
       strictEqual(
         consoleLogMock.mock.calls[0].arguments[0],
         "Opening $EDITOR in worktree 'docs'...",
